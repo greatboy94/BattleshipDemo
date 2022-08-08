2a5cfd4ae5d6516137d3e44b10f2234a0061e10b
Buiukbek Navruzov <Buiukbek.Navruzov@itechart-group.com>
2022-08-08 12:58:44 +0500

    MarkX around killed ships v2

--- BattleshipDemo/BattleshipField.cs
+++ BattleshipDemo/BattleshipField.cs
@@ -11,114 +11,96 @@ namespace BattleshipDemo
         public static int[,] show2 = new int[6, 6];
         
 
+        //Place ships to the field
         public void PlaceShips(string playerName, int[,] field)
         {
+            for(int deckCount =1; deckCount <=3; deckCount++){
+                string myDeck = shipTypes(deckCount);
+                Console.WriteLine($"{playerName} -> Please place your {myDeck} deck ship [You have only {deckCount}]");
+                BattleArea(field);
             
-            Console.WriteLine($"{playerName} -> Please place your triple deck ship [You have only 1]");
-            BattleArea(field);
+                for (int i = deckCount; i > 0; i--)
+                {
+                    n1:
+                    Console.WriteLine("Remaining ships: " + i);
+                    Console.WriteLine("Please enter X index coordinate:");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter Y index coordinate:");
+                    int y = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Place your ships:");
+                    Console.WriteLine("1. Vertical");
+                    Console.WriteLine("2. Horizontal");
+                    int line = Convert.ToInt32(Console.ReadLine());
+
+                    if(!checkEnabled(line,x,y,deckCount,field)){
+                        BattleArea(field);
+                        goto n1;
+                    }
+
+                    for (int j = 0; j < 4 - deckCount; j++)
+                    {
             
-            int tripleDeck = 3;
-            for (int i = 1; i > 0; i--)
-            {
-                Console.WriteLine("Remaining ships: " + i);
-                Console.WriteLine("Please enter X index coordinate:");
-                int x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Please enter Y index coordinate:");
-                int y = Convert.ToInt32(Console.ReadLine());
-                
-                Console.WriteLine("Place your ships:");
-                Console.WriteLine("1. Vertical");
-                Console.WriteLine("2. Horizontal");
-                int line = Convert.ToInt32(Console.ReadLine());
-
-                for (int j = 0; j < tripleDeck; j++)
-                {
-                    if (line==1)
-                    {
-                        field[x, y + j] = 1;
+                        if (line==1)
+                        {
+                            field[x, y + j] = 1;
+                    
+                        }
+                        else
+                        {
+                            field[x + j, y] = 1;
+                        }
                     }
-                    else
-                    {
-                        field[x + j, y] = 1;
-                    }
+                    BattleArea(field);
                 }
-                BattleArea(field);
-
-            }
-
-            int doubleDeck = 2;
-            Console.WriteLine($"{playerName} -> Please place your double deck ships [You have only 2]");
-            for (int i = 2; i > 0; i--)
-            {
-                repeat:
-                Console.WriteLine("Remaining ships: " + i);
-                Console.WriteLine("Please enter X index coordinate:");
-                int x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Please enter Y index coordinate:");
-                int y = Convert.ToInt32(Console.ReadLine());
-                
-                Console.WriteLine("Place your ships:");
-                Console.WriteLine("1. Vertical");
-                Console.WriteLine("2. Horizontal");
-                int line = Convert.ToInt32(Console.ReadLine());
-
-                if (!CheckNearbyShips(x,y,doubleDeck,line,field))
-                {
-                    Console.WriteLine("This field is not empty");
-                    goto repeat;
-                }
-
-                for (int j = 0; j < doubleDeck; j++)
-                {
-                    if (line==1)
-                    {
-                        field[x, y + j] = 1;
-                    }
-                    else
-                    {
-                        field[x + j, y] = 1;
-                    }
-                }
-                BattleArea(field);
-            }
-
-            int singleDeck = 1;
-            Console.WriteLine($"{playerName} -> Please place your single deck ships [You have only 3]");
-            for (int i = 3; i > 0; i--)
-            {
-                repeat:
-                Console.WriteLine("Remaining ships: " + i);
-                Console.WriteLine("Please enter X index coordinate:");
-                int x = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Please enter Y index coordinate:");
-                int y = Convert.ToInt32(Console.ReadLine());
-                
-                Console.WriteLine("Place your ships:");
-                Console.WriteLine("1. Vertical");
-                Console.WriteLine("2. Horizontal");
-                int line = Convert.ToInt32(Console.ReadLine());
-                
-                if (!CheckNearbyShips(x,y,singleDeck,line,field))
-                {
-                    Console.WriteLine("This field is not empty");
-                    goto repeat;
-                }
-
-                for (int j = 0; j < singleDeck; j++)
-                {
-                    if (line==1)
-                    {
-                        field[x, y + j] = 1;
-                    }
-                    else
-                    {
-                        field[x + j, y] = 1;
-                    }
-                }
-                BattleArea(field);
             }
         }
         
+        //This for getting ship types
+        public string shipTypes(int i)
+        {
+            switch (i)
+            {
+                case 1: return "tripple";
+                case 2: return "double";
+                case 3: return "single";
+                
+                default: return "none";
+            }
+        }
+        
+        //Checking for out of bound exception, if will try triple deck place to [5,5] coordinate for example. 
+        public bool checkEnabled(int line, int x, int y, int deckCount, int[,] field)
+        {
+          
+            if( ((line == 1 ? y : x) + (4 - deckCount)) > 6  || x < 0 || y < 0)
+            {
+                System.Console.WriteLine("Out of bound: Please reenter this deck");
+                return false;
+            }
+
+            //Checking for vertical, horizontal and diagonal
+            for (int i = -1; i <= deckCount; i++)
+            {
+                if(line == 1)
+                {
+                    if(checkZone(y,i) && (checkZone(x,-1) && (field[x-1,y+i] == 1) || field[x,y+i] == 1|| (checkZone(x,1) && field[x+1,y+i] == 1)))
+                    {
+                        System.Console.WriteLine("Can't place nearby, please find another place!");
+                        return false;
+                    }
+                }else{
+                    if(checkZone(x,i) && (checkZone(y,-1) && (field[x+i,y-1] == 1 )|| field[x+i,y] == 1 || (checkZone(y,1) && field[x+i,y+1] == 1)))
+                    {
+                        System.Console.WriteLine("Can't place nearby, please find another place!");
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        
+        //This is for drawing BattleArea array[6,6]
         public void BattleArea(int[,] field)
         {
             Console.WriteLine("   0   1   2   3   4   5");
@@ -141,7 +123,7 @@ namespace BattleshipDemo
       
         }
      
-        
+        //This is for starting game for attacking to ships
         public static void Fire(string playerName, int[,] show, int[,] field)
         {
             bool isBreak = false;
@@ -189,8 +171,6 @@ namespace BattleshipDemo
                     else
                     {
                         Console.WriteLine("Killed");
-                        //show[x + 1, y + 1] = 1;
-                        //show[x - 1, y - 1] = 1;
                         MarkX(x, y, field, show);
                     }
                     show[x, y] = 2;
@@ -212,6 +192,8 @@ namespace BattleshipDemo
                 }
             }
         }
+        
+        //This is for checking winner of the user after killed all ships.
       public static int CheckWinner()
         {
             int count1 = 0;
@@ -250,165 +232,132 @@ namespace BattleshipDemo
 
             return 0;
         }
-        
-        public static bool CheckNearbyShips(int x, int y, int shipType, int line, int[,] field)
+
+
+      //This is for checking user killed or injured any ships.
+      public static bool CheckKilledOrInjured(int x, int y, int[,] field, int[,] show)
         {
-            if (line==1)
-            {
-                if (y+shipType>field.GetLength(0))
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-
-            if (line==2)
-            {
-                if (x+shipType>field.GetLength(1))
-                {
-                    return false;
-                }
-                else
-                {
-                    return true;
-                }
-            }
-            
-            while (shipType!=0)
-            {
-                
-                for (int i = 0; i < shipType; i++)
-                {
-                    int storeX = 0;
-                    int storeY = 0;
-                    if (line==1)
-                    {
-                        storeY = i;
-                    }
-                    else
-                    {
-                        storeX = i;
-                    }
-        
-                    if (x+1+storeX<field.Length && x+1+storeX>=0)
-                    {
-                        if (field[x+1+storeX, y+storeY]!=0)
-                        {
-                            return false;
-                        }
-                    }
-                    if (x-1+storeX<field.Length && x-1+storeX>=0)
-                    {
-                        if (field[x-1+storeX, y+storeY]!=0)
-                        {
-                            return false;
-                        }
-                    }
-                    
-                    if (y+1+storeY<field.Length && y+1+storeY>=0)
-                    {
-                        if (field[x+storeX, y+1+storeY]!=0)
-                        {
-                            return false;
-                        }
-                    }
-                    if (y-1+storeY<field.Length && y-1+storeY>=0)
-                    {
-                        if (field[x+storeX, y-1+storeY]!=0)
-                        {
-                            return false;
-                        }
-                    }
-                }
-
-                return true;
-            }
-            return true;
-        }
-
-        public static bool CheckKilledOrInjured(int x, int y, int[,] field, int[,] show)
-        {
-            if (x<6 && field[x+1, y]==1 && show[x+1, y]!=2)
+            if (x+1<=5 && field[x+1, y]==1 && show[x+1, y]!=2)
             {
                 return true;
             }
 
-            if (x>1 && field[x-1, y]==1 && show[x-1, y]!=2)
+            if (x-1>=0 && field[x-1, y]==1 && show[x-1, y]!=2)
             {
                 return true;
             }
-            if (y<6 && field[x, y+1]==1 && show[x, y+1]!=2)
+            if (y+1<=5 && field[x, y+1]==1 && show[x, y+1]!=2)
             {
                 return true;
             }
-            if (y>1 && field[x, y-1]==1 && show[x, y-1]!=2)
+            if (y-1>=0 && field[x, y-1]==1 && show[x, y-1]!=2)
             {
                 return true;
             }
 
             return false;
         }
+      
+        //This is for marking X around killed ships.
         public static void MarkX(int x, int y, int[,] field, int[,] show)
         {
-            int storeX=-1;
-            int storeY=-1;
-
-            bool isHorizontal;
-            int countDecrement = 1;
-            while (true)
-            {
-                
-                if (x - countDecrement < 0 )
-                {
-                    storeX = x;
-                }
-                if (y - countDecrement < 0)
-                {
-                    storeY = y;
-                }
-                if (x-countDecrement > 0 && field[x-countDecrement,y]!=1)
-                {
-                    storeX = x - countDecrement;
-                }
-                if (y-countDecrement >0 && field[x,y-countDecrement]!=1)
-                {
-                    storeY = y - countDecrement;
-                }
-
-                countDecrement++;
-                if (storeX!=-1 && storeY!=-1)
-                {
-                    break;
-                }
-            }
+            bool isHorizontal = false;
+            bool isFirst = false;
+            bool isLast = false;
             
-            if (field[x+1, y]==2 || (x-1 > 0 && field[x - 1, y] == 2))
+            
+            //First checking for inside zone and for horizontal
+            if(checkZone(x,-1) && field[x-1,y] == 1 || checkZone(x,1) && field[x+1,y] == 1)
             {
                 isHorizontal = true;
             }
+
+            if(isHorizontal && (checkZone(x,-1) && field[x-1,y] != 1 || !checkZone(x,-1)))
+            {
+                isFirst = true;
+            } 
+            if(!isHorizontal && (checkZone(y,-1) && field[x,y-1] != 1 || !checkZone(y,-1)))
+            {
+                isFirst = true;
+            }
+            
+            if(isHorizontal && (checkZone(x,1) && field[x+1,y] != 1 || !checkZone(x,1)))
+            {
+                isLast = true;
+            } 
+            if(!isHorizontal && (checkZone(y,1) && field[x,y+1] != 1 || !checkZone(y,1)))
+            {
+                isLast = true;
+            }
+
+            //if deck's starting point first and last, this is single deck and need to fill matrix[3,3]
+            if(isFirst && isLast){
+                for(int i = -1; i< 2; i++)
+                {
+                    for(int j=-1;j<2;j++)
+                    {
+                        if(checkZone(x,i) && checkZone(y,j))
+                        {
+                            show[x+i,y+j] =1;
+                        }
+                    }
+                }
+            }
+            else if(isFirst)
+            {
+                for(int i=-1; i < (isHorizontal ? 4 : 2) ; i++)
+                {
+                    for (int j = -1; j < (isHorizontal ? 2 : 4); j++)
+                    {
+                        if(checkZone(x,i) && checkZone(y,j))
+                        {
+                            if(field[x+i,y+j] != 1)
+                            {
+                                if(j==2 && field[x,y+j-1]==1 || j!=2)
+                                {
+                                    if(j==3 && field[x,y+j-1]==1 || j!=3)
+                                    {
+                                        show[x+i,y+j] =1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if(isLast){
+                for(int i=-1; i < (isHorizontal ? 4 : 2) ; i++)
+                {
+                    for (int j = -1; j < (isHorizontal ? 2 : 4); j++)
+                    {
+                        if(checkZone(x,i) && checkZone(y,-j))
+                        {
+                            if(field[x+i,y-j] != 1)
+                            {
+                                if(j==2 && field[x,y-j+1]==1 || j!=2)
+                                {
+                                    if(j==3 && field[x,y-j+1]==1 || j!=3)
+                                    {
+                                        show[x+i,y-j] =1;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        //Checking for inside of zone
+        public static bool checkZone(int x, int i)
+        {
+            if(x+i >= 0 && x+i <= 5)
+            {
+                return true;
+            }
             else
             {
-                isHorizontal = false;
-            }
-
-            int shipLength = 0;
-            for (int i = 0; i < 3; i++)
-            {
-                if((storeX+1+i < 6 && storeY+1+i<6) && field[isHorizontal ? (storeX+1+i) : (storeX+1) ,isHorizontal ? (storeY+1) : (storeY+1+i)] == 1)
-                {
-                    shipLength++;
-                }
-            }
-
-            for (int i = 0; i < (isHorizontal ? (shipLength+2) : 3); i++)
-            {
-                for (int j = 0; j < (isHorizontal? 3: (shipLength+2)); j++)
-                {
-                    show[storeX+i,storeY+j] = 1;
-                }
+                return false;
             }
         }
     }
