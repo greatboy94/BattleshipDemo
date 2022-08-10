8375648c441acbf16dc5a642cc30d857d9ba1dbc
greatboy_94@mail.ru <greatboy_94@mail.ru>
2022-08-10 18:00:21 +0500

    fixed in comments

--- BattleshipDemo/BattleshipField.cs
+++ BattleshipDemo/BattleshipField.cs
@@ -14,7 +14,7 @@ namespace BattleshipDemo
         //Place ships to the field
         public void PlaceShips(string playerName, int[,] field)
         {
-            for(int deckCount =1; deckCount <=3; deckCount++){
+            for (int deckCount =1; deckCount <=3; deckCount++){
                 string myDeck = shipTypes(deckCount);
                 Console.WriteLine($"{playerName} -> Please place your {myDeck} deck ship [You have only {deckCount}]");
                 BattleArea(field);
@@ -33,23 +33,14 @@ namespace BattleshipDemo
                     Console.WriteLine("2. Horizontal");
                     int line = Convert.ToInt32(Console.ReadLine());
 
-                    if(!checkEnabled(line,x,y,deckCount,field)){
+                    if (!checkEnabled(line, x, y, deckCount, field)){
                         BattleArea(field);
                         goto n1;
                     }
 
                     for (int j = 0; j < 4 - deckCount; j++)
                     {
-            
-                        if (line==1)
-                        {
-                            field[x, y + j] = 1;
-                    
-                        }
-                        else
-                        {
-                            field[x + j, y] = 1;
-                        }
+                        int result = line == 1 ? field[x, y + j] = 1 : field[x + j, y] = 1;
                     }
                     BattleArea(field);
                 }
@@ -61,7 +52,7 @@ namespace BattleshipDemo
         {
             switch (i)
             {
-                case 1: return "tripple";
+                case 1: return "triple";
                 case 2: return "double";
                 case 3: return "single";
                 
@@ -72,27 +63,29 @@ namespace BattleshipDemo
         //Checking for out of bound exception, if will try triple deck place to [5,5] coordinate for example. 
         public bool checkEnabled(int line, int x, int y, int deckCount, int[,] field)
         {
-          
-            if( ((line == 1 ? y : x) + (4 - deckCount)) > 6  || x < 0 || y < 0)
+            
+            if (((line == 1 ? y : x) + (4 - deckCount)) > 6  || x < 0 || y < 0)
             {
-                System.Console.WriteLine("Out of bound: Please reenter this deck");
+                Console.WriteLine("Out of bound: Please reenter this deck");
                 return false;
             }
 
             //Checking for vertical, horizontal and diagonal
             for (int i = -1; i <= deckCount; i++)
             {
-                if(line == 1)
+                if (line == 1)
                 {
-                    if(checkZone(y,i) && (checkZone(x,-1) && (field[x-1,y+i] == 1) || field[x,y+i] == 1|| (checkZone(x,1) && field[x+1,y+i] == 1)))
+                    //(checkZone(y,i) here is checking if y+number y>6 and y<0;
+                    if (checkZone(y,i) && (checkZone(x,-1) && (field[x-1,y+i] == 1) || field[x,y+i] == 1|| (checkZone(x,1) && field[x+1,y+i] == 1)))
                     {
-                        System.Console.WriteLine("Can't place nearby, please find another place!");
+                        Console.WriteLine("Can't place nearby, please find another place!");
                         return false;
                     }
-                }else{
-                    if(checkZone(x,i) && (checkZone(y,-1) && (field[x+i,y-1] == 1 )|| field[x+i,y] == 1 || (checkZone(y,1) && field[x+i,y+1] == 1)))
+                }
+                else {
+                    if (checkZone(x,i) && (checkZone(y,-1) && (field[x+i,y-1] == 1 )|| field[x+i,y] == 1 || (checkZone(y,1) && field[x+i,y+1] == 1)))
                     {
-                        System.Console.WriteLine("Can't place nearby, please find another place!");
+                        Console.WriteLine("Can't place nearby, please find another place!");
                         return false;
                     }
                 }
@@ -149,7 +142,6 @@ namespace BattleshipDemo
                             Console.Write("[*] ");
                         }
                     }
-
                     Console.WriteLine();
                 }
 
@@ -162,6 +154,7 @@ namespace BattleshipDemo
                 int x = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please enter Y index coordinates:");
                 int y = Convert.ToInt32(Console.ReadLine());
+                
                 if (field[x, y] == 1)
                 {
                     if (CheckKilledOrInjured(x,y,field,show))
@@ -175,13 +168,13 @@ namespace BattleshipDemo
                     }
                     show[x, y] = 2;
                 }
+                
                 else
                 {
                     Console.WriteLine("Miss, wait your next move");
                     show[x, y] = 1;
                     Console.WriteLine("Please hit enter for next player turn");
                     Console.ReadKey();
-                    isBreak = true;
                     Console.Clear();
                     break;
                 }
@@ -225,11 +218,11 @@ namespace BattleshipDemo
             {
                 return 1;
             }
+            
             if (count2>=10)
             {
                 return 2;
             }
-
             return 0;
         }
 
@@ -246,15 +239,16 @@ namespace BattleshipDemo
             {
                 return true;
             }
+            
             if (y+1<=5 && field[x, y+1]==1 && show[x, y+1]!=2)
             {
                 return true;
             }
+            
             if (y-1>=0 && field[x, y-1]==1 && show[x, y-1]!=2)
             {
                 return true;
             }
-
             return false;
         }
       
@@ -265,57 +259,56 @@ namespace BattleshipDemo
             bool isFirst = false;
             bool isLast = false;
             
-            
             //First checking for inside zone and for horizontal
-            if(checkZone(x,-1) && field[x-1,y] == 1 || checkZone(x,1) && field[x+1,y] == 1)
+            if (checkZone(x,-1) && field[x-1,y] == 1 || checkZone(x,1) && field[x+1,y] == 1)
             {
                 isHorizontal = true;
             }
 
-            if(isHorizontal && (checkZone(x,-1) && field[x-1,y] != 1 || !checkZone(x,-1)))
+            if (isHorizontal && (checkZone(x,-1) && field[x-1,y] != 1 || !checkZone(x,-1)))
             {
                 isFirst = true;
             } 
-            if(!isHorizontal && (checkZone(y,-1) && field[x,y-1] != 1 || !checkZone(y,-1)))
+            if (!isHorizontal && (checkZone(y,-1) && field[x,y-1] != 1 || !checkZone(y,-1)))
             {
                 isFirst = true;
             }
             
-            if(isHorizontal && (checkZone(x,1) && field[x+1,y] != 1 || !checkZone(x,1)))
+            if (isHorizontal && (checkZone(x,1) && field[x+1,y] != 1 || !checkZone(x,1)))
             {
                 isLast = true;
             } 
-            if(!isHorizontal && (checkZone(y,1) && field[x,y+1] != 1 || !checkZone(y,1)))
+            if (!isHorizontal && (checkZone(y,1) && field[x,y+1] != 1 || !checkZone(y,1)))
             {
                 isLast = true;
             }
 
             //if deck's starting point first and last, this is single deck and need to fill matrix[3,3]
-            if(isFirst && isLast){
-                for(int i = -1; i< 2; i++)
+            if (isFirst && isLast){
+                for (int i = -1; i< 2; i++)
                 {
-                    for(int j=-1;j<2;j++)
+                    for (int j=-1;j<2;j++)
                     {
-                        if(checkZone(x,i) && checkZone(y,j))
+                        if (checkZone(x,i) && checkZone(y,j))
                         {
                             show[x+i,y+j] =1;
                         }
                     }
                 }
             }
-            else if(isFirst)
+            else if (isFirst)
             {
-                for(int i=-1; i < (isHorizontal ? 4 : 2) ; i++)
+                for (int i=-1; i < (isHorizontal ? 4 : 2) ; i++)
                 {
                     for (int j = -1; j < (isHorizontal ? 2 : 4); j++)
                     {
-                        if(checkZone(x,i) && checkZone(y,j))
+                        if (checkZone(x,i) && checkZone(y,j))
                         {
-                            if(field[x+i,y+j] != 1)
+                            if (field[x+i,y+j] != 1)
                             {
-                                if((isHorizontal ? i==2 : j==2) && field[x+(isHorizontal ? i-1 : 0),y+(isHorizontal ? 0 : j-1)]==1 || (isHorizontal ? i!=2 : j!=2))
+                                if ((isHorizontal ? i==2 : j==2) && field[x+(isHorizontal ? i-1 : 0),y+(isHorizontal ? 0 : j-1)]==1 || (isHorizontal ? i!=2 : j!=2))
                                 {
-                                    if((isHorizontal ? i==3 : j==3) && field[x+(isHorizontal ? i-1 : 0),y+(isHorizontal ? 0 : j-1)]==1 || (isHorizontal ?i!=3 : j!=3))
+                                    if ((isHorizontal ? i==3 : j==3) && field[x+(isHorizontal ? i-1 : 0),y+(isHorizontal ? 0 : j-1)]==1 || (isHorizontal ?i!=3 : j!=3))
                                     {
                                         show[x+i,y+j] =1;
                                     }
@@ -325,18 +318,18 @@ namespace BattleshipDemo
                     }
                 }
             }
-            else if(isLast){
-                for(int i=-1; i < (isHorizontal ? 4 : 2) ; i++)
+            else if (isLast){
+                for (int i=-1; i < (isHorizontal ? 4 : 2) ; i++)
                 {
                     for (int j = -1; j < (isHorizontal ? 2 : 4); j++)
                     {
-                        if(checkZone(x,-i) && checkZone(y,-j))
+                        if (checkZone(x,-i) && checkZone(y,-j))
                         {
-                            if(field[x-i,y-j] != 1)
+                            if (field[x-i,y-j] != 1)
                             {
-                                if((isHorizontal ? i==2 : j==2) && field[x - (isHorizontal ?i-1:0),y - (isHorizontal ?0:j-1)]==1 || (isHorizontal ?i!=2 : j!=2))
+                                if ((isHorizontal ? i==2 : j==2) && field[x - (isHorizontal ?i-1:0),y - (isHorizontal ?0:j-1)]==1 || (isHorizontal ?i!=2 : j!=2))
                                 {
-                                    if((isHorizontal ?i==3 : j==3) && field[x - (isHorizontal ?i-1:0),y + (isHorizontal ?0:j-1)]==1 || (isHorizontal ?i!=3 : j!=3))
+                                    if ((isHorizontal ?i==3 : j==3) && field[x - (isHorizontal ?i-1:0),y + (isHorizontal ?0:j-1)]==1 || (isHorizontal ?i!=3 : j!=3))
                                     {
                                         show[x-i,y-j] =1;
                                     }
@@ -351,7 +344,7 @@ namespace BattleshipDemo
         //Checking for inside of zone
         public static bool checkZone(int x, int i)
         {
-            if(x+i >= 0 && x+i <= 5)
+            if (x+i >= 0 && x+i <= 5)
             {
                 return true;
             }
